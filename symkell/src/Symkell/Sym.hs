e34454a563ac0868fcbcb7adaad7560199784ff1
Nasibullin Alan Maratovich <alan.nasibyllin@yandex.ru>
2025-04-24 10:09:47 +0000

    fix

--- symkell/src/Symkell/Sym.hs
+++ symkell/src/Symkell/Sym.hs
@@ -45,7 +45,7 @@ where
 import Control.DeepSeq (NFData)
 import Data.Ratio
 import Data.String (IsString, fromString)
-import Data.Text
+import Data.Text (Text, pack)
 import GHC.Generics (Generic)
 import TextShow (TextShow)
 import TextShow.Generic (FromGeneric (..))
@@ -64,7 +64,6 @@ data Expression
 
 pattern Pi' :: Expression
 pattern Pi' = Symbol "pi"
-
 
 data UnaryFunction
   = Negate | Abs | Signum | Exp | Log | Sqrt | Sin | Cos | Tan | Asin | Acos | Atan | Sinh | Cosh | Tanh | Asinh | Acosh | Atanh
@@ -90,7 +89,6 @@ pattern Tanh' x = UnaryApply Tanh x
 pattern Asinh' x = UnaryApply Asinh x
 pattern Acosh' x = UnaryApply Acosh x
 pattern Atanh' x = UnaryApply Atanh x
-
 
 data BinaryFunction
   = Add | Multiply | Subtract | Divide | Power | LogBase
@@ -144,7 +142,6 @@ instance Floating Expression where
   acosh = UnaryApply Acosh
   atanh = UnaryApply Atanh
 
-
 getUnaryFunction :: (Floating a) => UnaryFunction -> (a -> a)
 getUnaryFunction Negate = negate
 getUnaryFunction Abs = abs
@@ -154,93 +151,4 @@ getUnaryFunction Log = log
 getUnaryFunction Sqrt = sqrt
 getUnaryFunction Sin = sin
 getUnaryFunction Cos = cos
-getUnaryFunction Tan = tan
-getUnaryFunction Asin = asin
-getUnaryFunction Acos = acos
-getUnaryFunction Atan = atan
-getUnaryFunction Sinh = sinh
-getUnaryFunction Cosh = cosh
-getUnaryFunction Tanh = tanh
-getUnaryFunction Asinh = asinh
-getUnaryFunction Acosh = acosh
-getUnaryFunction Atanh = atanh
-
-
-getBinaryFunction :: (Floating a) => BinaryFunction -> (a -> a -> a)
-getBinaryFunction Add = (+)
-getBinaryFunction Multiply = (*)
-getBinaryFunction Subtract = (-)
-getBinaryFunction Divide = (/)
-getBinaryFunction Power = (**)
-getBinaryFunction LogBase = logBase
-
-
-substitute ::
-  Expression ->
-  (Text -> Maybe Expression) ->
-  Expression
-substitute e@(Number _) _ = e
-substitute e@(Symbol s) f
-  | (Just x) <- f s = x
-  | otherwise = e
-substitute (UnaryApply func x) f = UnaryApply func (substitute x f)
-substitute (BinaryApply func x y) f = BinaryApply func (substitute x f) (substitute y f)
-
-
-evaluate ::
-  (Floating a) =>
-  Expression ->
-  (Text -> Maybe a) ->
-  Maybe a
-evaluate (Number n) _ = Just $ fromInteger n
-evaluate (Symbol "pi") _ = Just pi
-evaluate (Symbol x) m = m x
-evaluate (UnaryApply fun expr) m = fmap f v
-  where
-    f = getUnaryFunction fun
-    v = evaluate expr m
-evaluate (BinaryApply fun expr1 expr2) m = f <$> v1 <*> v2
-  where
-    f = getBinaryFunction fun
-    v1 = evaluate expr1 m
-    v2 = evaluate expr2 m
-
-
-fractionalEvaluate ::
-  (Eq a, Fractional a) =>
-  Expression ->
-  (Text -> Maybe a) ->
-  Maybe a
-fractionalEvaluate (Number n) _ = Just $ fromInteger n
-fractionalEvaluate (Symbol x) m = m x
-fractionalEvaluate (Negate' x) m = negate <$> fractionalEvaluate x m
-fractionalEvaluate (Abs' x) m = abs <$> fractionalEvaluate x m
-fractionalEvaluate (Signum' x) m = signum <$> fractionalEvaluate x m
-fractionalEvaluate (x :+: y) m = (+) <$> fractionalEvaluate x m <*> fractionalEvaluate y m
-fractionalEvaluate (x :-: y) m = (-) <$> fractionalEvaluate x m <*> fractionalEvaluate y m
-fractionalEvaluate (x :*: y) m = (*) <$> fractionalEvaluate x m <*> fractionalEvaluate y m
-fractionalEvaluate (x :/: y) m
-  | Just 0 <- y' = Nothing
-  | otherwise = (/) <$> x' <*> y'
-  where
-    x' = fractionalEvaluate x m
-    y' = fractionalEvaluate y m
-fractionalEvaluate (x :**: (Number n)) m = (^^ n) <$> fractionalEvaluate x m
-fractionalEvaluate _ _ = Nothing
-
-toFunction ::
-  (Floating b) =>
-  Expression ->
-  (Text -> (a -> b)) ->
-  (a -> b)
-toFunction (Number n) _ = const $ fromInteger n
-toFunction (Symbol s) m = m s
-toFunction (UnaryApply func x) m = f . g
-  where
-    f = getUnaryFunction func
-    g = toFunction x m
-toFunction (BinaryApply func x y) m = \v -> f (g v) (h v)
-  where
-    f = getBinaryFunction func
-    g = toFunction x m
-    h = toFunction y m
+getUnaryFunction Tan
